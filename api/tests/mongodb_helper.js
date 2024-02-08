56fe77dc5024f59d0379e5d7bc32a63a22a64918
dan gullis <daniel.gullis1@gmail.com>
2024-02-08 23:19:54 +0000

    edit a

--- api/tests/mongodb_helper.js
+++ api/tests/mongodb_helper.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
-const { connectToDatabase } = require("../db/db");
+// const mongoose = require("mongoose");
+// const { connectToDatabase } = require("../db/db");
 
-beforeAll(async () => {
-  await connectToDatabase();
-});
+// beforeAll(async () => {
+//   await connectToDatabase();
+// });
 
-afterAll(async () => {
-  await mongoose.connection.close(true);
-});
+// afterAll(async () => {
+//   await mongoose.connection.close(true);
+// });
